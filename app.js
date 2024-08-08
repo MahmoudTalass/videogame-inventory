6d811659e8247efbb9a28815858f84d83ec357bb
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-08-08 19:25:03 -0400

    Attach routes to app

--- app.js
+++ app.js
@@ -5,5 +5,17 @@ require("dotenv").config();
 
 app.set("view engine", "ejs");
 
+const indexRouter = require("./routes/index.route");
+const developersRouter = require("./routes/developers.route");
+const gamesRouter = require("./routes/games.route");
+const genresRouter = require("./routes/genres.route");
+const platformsRouter = require("./routes/platform.routes");
+
+app.use("/", indexRouter);
+app.use("/developers", developersRouter);
+app.use("/games", gamesRouter);
+app.use("/genres", genresRouter);
+app.use("/platforms", platformsRouter);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
