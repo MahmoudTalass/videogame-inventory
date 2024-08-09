56501b90633d84b19f7afff110935a2379b663c7
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-08-08 20:10:07 -0400

    Use form parsing middleware and make /home default route

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 require("dotenv").config();
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 const indexRouter = require("./routes/index.route");
 const developersRouter = require("./routes/developers.route");
@@ -11,11 +13,13 @@ const gamesRouter = require("./routes/games.route");
 const genresRouter = require("./routes/genres.route");
 const platformsRouter = require("./routes/platform.routes");
 
-app.use("/", indexRouter);
+app.use("/home", indexRouter);
 app.use("/developers", developersRouter);
 app.use("/games", gamesRouter);
 app.use("/genres", genresRouter);
 app.use("/platforms", platformsRouter);
+
+app.get("/", (req, res) => res.redirect("/home"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
