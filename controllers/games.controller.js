0a5a1487fd263130a1e909bb3f3efdf7d35e02a5
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-08-12 14:40:51 -0400

    Create controller functions for handling game creation form

--- controllers/games.controller.js
+++ controllers/games.controller.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
-const { GameService } = require("../db/query");
+const { GameService, PlatformService, GenreService, DeveloperService } = require("../db/query");
+const { validateGameInput } = require("../middlwares/validation");
 
 const getAllGames = asyncHandler(async (req, res) => {
    const games = await GameService.getAllGames();
@@ -14,7 +15,33 @@ const getGame = asyncHandler(async (req, res) => {
    // display in view
 });
 
+const createGameGet = asyncHandler(async (req, res) => {
+   const [genres, developers, platforms] = await Promise.all([
+      GenreService.getAllGenres(),
+      DeveloperService.getAllDevelopers(),
+      PlatformService.getAllPlatforms(),
+   ]);
+
+   res.render("create-game-form", {
+      title: "Add New Game",
+      genres,
+      developers,
+      platforms,
+   });
+});
+
+const createGamePost = [
+   validateGameInput,
+   asyncHandler(async (req, res) => {
+      await GameService.createGame(req.params);
+
+      res.redirect("/games");
+   }),
+];
+
 module.exports = {
    getAllGames,
    getGame,
+   createGameGet,
+   createGamePost,
 };
